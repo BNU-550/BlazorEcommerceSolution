b5f376c9fd5e56aea7422a54cbab456da5174594
Derek Peacock <derek.peacock@yahoo.com>
2022-02-23 10:27:14 +0000

    Added more data and categories

--- BlazorEcommerceProject/Server/Data/DbContext.cs
+++ BlazorEcommerceProject/Server/Data/DbContext.cs
@@ -11,6 +11,12 @@
         /// </summary>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Books", URL = "books" },
+                new Category { Id = 2, Name = "Movies", URL = "movies" },
+                new Category { Id = 3, Name = "Video Games", URL = "video-games" }
+                );
+
             modelBuilder.Entity<Product>().HasData(
                 new Product
                 {
@@ -18,7 +24,8 @@
                     Title = "HitchHiker's Guide to the Galaxy",
                     Description = "The Hitchhiker's Guide to the Galaxy has become an international multi-media phenomenon; the novels are the most widely distributed, having been translated into more than 30 languages by 2005.[4][5] The first novel, The Hitchhiker's Guide to the Galaxy (1979), has been ranked fourth on the BBC’s The Big Read poll.[6] The sixth novel, And Another Thing, was written by Eoin Colfer with additional unpublished material by Douglas Adams. In 2017, BBC Radio 4 announced a 40th-anniversary celebration with Dirk Maggs, one of the original producers, in charge.[7] The first of six new episodes was broadcast on 8 March 2018.",
                     ImageURL = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
-                    Price = 9.99m
+                    Price = 9.99m,
+                    CategoryId = 1
                 },
                 new Product
                 {
@@ -26,7 +33,8 @@
                     Title = "Lord of the Rings",
                     Description = "The Lord of the Rings is an epic[1] high-fantasy novel[a] by English author and scholar J. R. R. Tolkien. Set in Middle-earth, intended to be Earth at some distant time in the past, the story began as a sequel to Tolkien's 1937 children's book The Hobbit, but eventually developed into a much larger work. Written in stages between 1937 and 1949, The Lord of the Rings is one of the best-selling books ever written, with over 150 million copies sold.",
                     ImageURL = "https://upload.wikimedia.org/wikipedia/en/e/e9/First_Single_Volume_Edition_of_The_Lord_of_the_Rings.gif",
-                    Price = 7.99m
+                    Price = 7.99m,
+                    CategoryId = 1
                 },
                 new Product
                 {
@@ -34,12 +42,78 @@
                     Title = "Dune",
                     Description = "Dune is a 1965 epic science fiction novel by American author Frank Herbert, originally published as two separate serials in Analog magazine. It tied with Roger Zelazny's This Immortal for the Hugo Award in 1966 and it won the inaugural Nebula Award for Best Novel. It is the first installment of the Dune saga. In 2003, it was described as the world's best-selling science fiction novel.",
                     ImageURL = "https://upload.wikimedia.org/wikipedia/en/d/de/Dune-Frank_Herbert_%281965%29_First_edition.jpg",
-                    Price = 6.99m
-                }
+                    Price = 6.99m,
+                    CategoryId = 1
+                },
+                    new Product
+                    {
+                        Id = 4,
+                        CategoryId = 2,
+                        Title = "The Matrix",
+                        Description = "The Matrix is a 1999 science fiction action film written and directed by the Wachowskis, and produced by Joel Silver. Starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano, and as the first installment in the Matrix franchise, it depicts a dystopian future in which humanity is unknowingly trapped inside a simulated reality, the Matrix, which intelligent machines have created to distract humans while using their bodies as an energy source. When computer programmer Thomas Anderson, under the hacker alias \"Neo\", uncovers the truth, he \"is drawn into a rebellion against the machines\" along with other people who have been freed from the Matrix.",
+                        ImageURL = "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+                    },
+                    new Product
+                    {
+                        Id = 5,
+                        CategoryId = 2,
+                        Title = "Back to the Future",
+                        Description = "Back to the Future is a 1985 American science fiction film directed by Robert Zemeckis. Written by Zemeckis and Bob Gale, it stars Michael J. Fox, Christopher Lloyd, Lea Thompson, Crispin Glover, and Thomas F. Wilson. Set in 1985, the story follows Marty McFly (Fox), a teenager accidentally sent back to 1955 in a time-traveling DeLorean automobile built by his eccentric scientist friend Doctor Emmett \"Doc\" Brown (Lloyd). Trapped in the past, Marty inadvertently prevents his future parents' meeting—threatening his very existence—and is forced to reconcile the pair and somehow get back to the future.",
+                        ImageURL = "https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg",
+                    },
+                    new Product
+                    {
+                        Id = 6,
+                        CategoryId = 2,
+                        Title = "Toy Story",
+                        Description = "Toy Story is a 1995 American computer-animated comedy film produced by Pixar Animation Studios and released by Walt Disney Pictures. The first installment in the Toy Story franchise, it was the first entirely computer-animated feature film, as well as the first feature film from Pixar. The film was directed by John Lasseter (in his feature directorial debut), and written by Joss Whedon, Andrew Stanton, Joel Cohen, and Alec Sokolow from a story by Lasseter, Stanton, Pete Docter, and Joe Ranft. The film features music by Randy Newman, was produced by Bonnie Arnold and Ralph Guggenheim, and was executive-produced by Steve Jobs and Edwin Catmull. The film features the voices of Tom Hanks, Tim Allen, Don Rickles, Wallace Shawn, John Ratzenberger, Jim Varney, Annie Potts, R. Lee Ermey, John Morris, Laurie Metcalf, and Erik von Detten. Taking place in a world where anthropomorphic toys come to life when humans are not present, the plot focuses on the relationship between an old-fashioned pull-string cowboy doll named Woody and an astronaut action figure, Buzz Lightyear, as they evolve from rivals competing for the affections of their owner, Andy Davis, to friends who work together to be reunited with Andy after being separated from him.",
+                        ImageURL = "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
 
+                    },
+                    new Product
+                    {
+                        Id = 7,
+                        CategoryId = 3,
+                        Title = "Half-Life 2",
+                        Description = "Half-Life 2 is a 2004 first-person shooter game developed and published by Valve. Like the original Half-Life, it combines shooting, puzzles, and storytelling, and adds features such as vehicles and physics-based gameplay.",
+                        ImageURL = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg"
+                    },
+                    new Product
+                    {
+                        Id = 8,
+                        CategoryId = 3,
+                        Title = "Diablo II",
+                        Description = "Diablo II is an action role-playing hack-and-slash computer video game developed by Blizzard North and published by Blizzard Entertainment in 2000 for Microsoft Windows, Classic Mac OS, and macOS.",
+                        ImageURL = "https://upload.wikimedia.org/wikipedia/en/d/d5/Diablo_II_Coverart.png",
+                    },
+                    new Product
+                    {
+                        Id = 9,
+                        CategoryId = 3,
+                        Title = "Day of the Tentacle",
+                        Description = "Day of the Tentacle, also known as Maniac Mansion II: Day of the Tentacle, is a 1993 graphic adventure game developed and published by LucasArts. It is the sequel to the 1987 game Maniac Mansion.",
+                        ImageURL = "https://upload.wikimedia.org/wikipedia/en/7/79/Day_of_the_Tentacle_artwork.jpg"
+                    },
+                    new Product
+                    {
+                        Id = 10,
+                        CategoryId = 3,
+                        Title = "Xbox",
+                        Description = "The Xbox is a home video game console and the first installment in the Xbox series of video game consoles manufactured by Microsoft.",
+                        ImageURL = "https://upload.wikimedia.org/wikipedia/commons/4/43/Xbox-console.jpg",
+                    },
+                    new Product
+                    {
+                        Id = 11,
+                        CategoryId = 3,
+                        Title = "Super Nintendo Entertainment System",
+                        Description = "The Super Nintendo Entertainment System (SNES), also known as the Super NES or Super Nintendo, is a 16-bit home video game console developed by Nintendo that was released in 1990 in Japan and South Korea.",
+                        ImageURL = "https://upload.wikimedia.org/wikipedia/commons/e/ee/Nintendo-Super-Famicom-Set-FL.jpg",
+                    }
             );
         }
 
         public DbSet<Product> Products { get; set;}
+        public DbSet<Category> Categories { get; set;}
     }
 }
---
+++ BlazorEcommerceProject/Shared/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorEcommerceProject.Shared
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = String.Empty;
+
+        public string URL { get; set; } = String.Empty;
+
+
+    }
+}
